c1b78032dd471cd4dbef8baf8cedc7b14fdb1ab2
Caitano <danielcaitano9@hotmail.com>
2020-06-19 20:51:32 -0300

    inicial

---
+++ calc.js
@@ -0,0 +1,7 @@
+$(function () {
+    $('#calc').click(function () {
+        const milhas = parseFloat($('#milhas').val())
+        const metros = milhas * 1609.34
+        $('#metros').val(metros)
+    })
+})  
