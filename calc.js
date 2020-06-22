c0478bde6bd0f29cfbcc895b5476001b02c3fbaf
Caitano <danielcaitano9@hotmail.com>
2020-06-21 22:51:39 -0300

    transformado HTML para EJS. Adcionando configurações do Vercel

--- calc.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $('#calc').click(function () {
-        const milhas = parseFloat($('#milhas').val())
-        const metros = milhas * 1609.34
-        $('#metros').val(metros)
-    })
-})  
